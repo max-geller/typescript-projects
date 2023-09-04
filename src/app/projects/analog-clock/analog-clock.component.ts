7356c0ed4714635c440367adadfe1dffcae9fbc3
Max Geller <mxgeller@gmail.com>
2023-09-04 15:54:10 -0700

    configuring clock face and hand movement

--- src/app/projects/analog-clock/analog-clock.component.ts
+++ src/app/projects/analog-clock/analog-clock.component.ts
@@ -1,64 +1,70 @@
-import { Component, OnInit } from "@angular/core";
-
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  Renderer2,
+  ViewChild,
+  ElementRef,
+} from "@angular/core";
 @Component({
   template: `
     <markdown clipboard lineNumbers [src]="info"></markdown>
-
-    <div class="clock">
-      <div class="clock-face">
-        <div class="hand hour-hand"></div>
-        <div class="hand min-hand"></div>
-        <div class="hand second-hand"></div>
+    <div class="project-container">
+      <div class="clock">
+        <div class="clock-face">
+          <div class=center></div>
+          <div class="hand hour-hand" #hourHand></div>
+          <div class="hand minute-hand" #minuteHand></div>
+          <div class="hand second-hand" #secondHand></div>
+        </div>
       </div>
     </div>
-
     <markdown clipboard lineNumbers [src]="overview"></markdown>
     <markdown clipboard lineNumbers [src]="code"></markdown>
   `,
   styleUrls: ["./analog-clock.component.css"],
 })
-export class AnalogClockComponent implements OnInit {
-  analogueClockConfig = {
-    baseColor: "#ffffff",
-    borderColor: "#000000",
-    borderWidth: 5,
-    centerColor: "#000000",
-    handColors: {
-      hourHand: "#000000",
-      minutHand: "#000000",
-      secondHand: "#000000",
-    },
-    notchColor: "#000000",
-    numbersColor: "#000000",
-    showNumbers: true,
-    size: 300,
-  };
+export class AnalogClockComponent implements AfterViewInit {
+  @ViewChild("secondHand", { static: true }) secondHand!: ElementRef;
+  @ViewChild("minuteHand", { static: true }) minuteHand!: ElementRef;
+  @ViewChild("hourHand", { static: true }) hourHand!: ElementRef;
+
   // Load Markdown and Code files
   info = "assets/projects/analog-clock/info.md";
   overview = "assets/projects/analog-clock/overview.md";
   code = "assets/projects/analog-clock/code.ts";
 
-  // App-Specific Variables
-  hourHand = document.querySelector(".hour-hand");
-  minuteHand = document.querySelector(".minute-hand");
-  secondHand = document.querySelector(".second-hand");
-
-  constructor() {}
-
-  async setDate() {
+  constructor(private renderer: Renderer2) {}
+  setTime() {
     const now = new Date();
 
     const seconds = now.getSeconds();
     const secondsDegrees = (seconds / 60) * 360 + 90;
+    this.renderer.setStyle(
+      this.secondHand.nativeElement,
+      "transform",
+      `rotate(${secondsDegrees}deg)`
+    );
 
-    const minutes = now.getMinutes();
-    const minutesDegrees = (minutes / 60) * 360 + 90;
+    const mins = now.getMinutes();
+    const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+    this.renderer.setStyle(
+      this.minuteHand.nativeElement,
+      "transform",
+      `rotate(${minsDegrees}deg)`
+    );
 
-    const hours = now.getHours();
-    const hoursDegrees = (hours / 12) * 360 + 90;
+    const hour = now.getHours();
+    const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+    this.renderer.setStyle(
+      this.hourHand.nativeElement,
+      "transform",
+      `rotate(${hourDegrees}deg)`
+    );
   }
 
-  ngOnInit() {
-    setInterval(this.setDate, 1000);
+  ngAfterViewInit() {
+    this.setTime();
+    setInterval(() => this.setTime(), 1000);
   }
 }
