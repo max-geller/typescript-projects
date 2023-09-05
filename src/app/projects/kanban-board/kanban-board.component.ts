c802ca89465077a2807a3e36cf05a20fe7dbc15e
Max Geller <mxgeller@gmail.com>
2023-09-04 17:50:20 -0700

    basic kanban columns

--- src/app/projects/kanban-board/kanban-board.component.ts
+++ src/app/projects/kanban-board/kanban-board.component.ts
@@ -1,17 +1,39 @@
 import { Component } from "@angular/core";
 
+
+
 @Component({
   template: ` <markdown clipboard lineNumbers [src]="info"></markdown>
-    <div class="project-container" style="height: 400px;">
-      <p>Project Container</p>
-    </div>
+<button (click)="addColumn()">Add Column</button>
+
+<div class="kanban-container" style="height: 400px;">
+  <div *ngFor="let column of columns" class="kanban-column">
+    {{ column.name }}
+    <button (click)="removeColumn(column.id)">X</button>
+    <!-- Add task cards here -->
+  </div>
+</div>
     <markdown clipboard lineNumbers [src]="overview"></markdown>
     <markdown clipboard lineNumbers [src]="code"></markdown>`,
 
   styleUrls: ["./kanban-board.component.css"],
 })
 export class KanbanBoardComponent {
+  columns = [
+    { id: 1, name: 'Pending' },
+    { id: 2, name: 'In Progress' },
+    { id: 3, name: 'Complete' },
+  ];
   info = "assets/projects/kanban-board/info.md";
   overview = "assets/projects/kanban-board/overview.md";
   code = "assets/projects/kanban-board/code.ts";
+
+  addColumn() {
+    const id = this.columns.length + 1;
+    this.columns.push({ id, name: `Column ${id}` });
+  }
+  
+  removeColumn(columnId: number) {
+    this.columns = this.columns.filter(column => column.id !== columnId);
+  }
 }
